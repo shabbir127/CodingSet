5eed800f863087473273456f7df3adab139ce6c5
shabbirkhan <khanshabbir126@gmail.com>
2026-01-11 21:30:54 +0530

    stream

--- src/com/java8/StreamAPIEx.java
+++ src/com/java8/StreamAPIEx.java
@@ -1,5 +1,6 @@
 package com.java8;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -24,7 +25,9 @@ public class StreamAPIEx {
 		
 		Stream<String> stream4=Stream.generate(()->"Shabbir").limit(5);
 		
+		List<Integer> list2=Arrays.asList(1,3,4,5,6,45,667,8,90,2);
 		
+		System.out.println(list2.stream().filter(i-> i % 2==0).toList());
 		
 		
 		
