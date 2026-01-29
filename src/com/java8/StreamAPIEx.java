06f2df0a3eb14ffb2d462d0f866bcd26d6c127fc
shabbirkhan <khanshabbir126@gmail.com>
2026-01-29 11:34:40 +0530

    commit

--- src/com/java8/StreamAPIEx.java
+++ src/com/java8/StreamAPIEx.java
@@ -3,6 +3,7 @@ package com.java8;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamAPIEx {
@@ -19,6 +20,7 @@ public class StreamAPIEx {
 		Stream<Integer> stream2=Stream.of(1,3,4,5,6);
 		
 		Stream<Integer> limit=Stream.iterate(0, n->n +1).limit(34);
+		
 		System.out.println(limit.toList());
 		
 		Stream<Double> stream3=Stream.generate(()->Math.random()*100).limit(5);
@@ -39,6 +41,10 @@ public class StreamAPIEx {
 	
 		
          System.out.println(all.stream().filter(i->i%2==0).map(i->i*i).reduce(0,Integer::sum));
-		
+         
+         
+         List<String>strings=Arrays.asList( "apple", "banana", " ", "banana", " ", "grapes", "apple", "banana");
+        
+         System.out.println(strings.stream().map(String::trim).filter(i->!i.isEmpty()).distinct().toList());
 	}
 }
