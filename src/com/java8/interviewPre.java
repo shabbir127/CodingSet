86007e655b142c545c7f4025aa543dd7fd05ed58
shabbirkhan <khanshabbir126@gmail.com>
2026-01-29 11:51:48 +0530

    commit

--- src/com/java8/interviewPre.java
+++ src/com/java8/interviewPre.java
@@ -2,4 +2,24 @@ package com.java8;
 
 public class interviewPre {
 
+	public static void main(String[] args) {
+		functionalinter ex=()->{System.out.println();};
+		ex.display();
+		
+		FunctionalInterfaceEx1 ex1=(a,b)->{System.out.println();};
+		ex1.sum(23, 34);
+	}
+}
+
+@FunctionalInterface
+interface functionalinter
+{
+	public void display();
+	
+}
+
+@FunctionalInterface
+interface FunctionalInterfaceEx1
+{
+	public void sum(int a,int b);
 }
