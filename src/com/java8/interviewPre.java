8bc48aa042893aae73e2e6de8acc94351bf9e0b2
shabbirkhan <khanshabbir126@gmail.com>
2026-01-29 12:06:26 +0530

    commit

--- src/com/java8/interviewPre.java
+++ src/com/java8/interviewPre.java
@@ -8,6 +8,14 @@ public class interviewPre {
 		
 		FunctionalInterfaceEx1 ex1=(a,b)->{System.out.println("Sum:- "+ (a+b));};
 		ex1.sum(23, 34);
+		
+		FunctionalInterfaceEx2 ex2=(a,b)->{ System.out.print("Mul:- ");
+			return a*b; };
+		System.out.print(ex2.mul(23, 34));
+		
+		
+		
+		
 	}
 }
 
@@ -15,11 +23,16 @@ public class interviewPre {
 interface functionalinter
 {
 	public void display();
-	
 }
 
 @FunctionalInterface
 interface FunctionalInterfaceEx1
 {
 	public void sum(int a,int b);
+}
+
+@FunctionalInterface
+interface FunctionalInterfaceEx2
+{
+	public int mul(int a,int b);
 }
