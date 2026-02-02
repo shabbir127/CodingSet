f1a0cdd80a30badbbeef7ae0c6934835bfd57786
shabbirkhan <khanshabbir126@gmail.com>
2026-02-02 23:57:30 +0530

    commit

--- src/com/java8/StreamQuestions.java
+++ src/com/java8/StreamQuestions.java
@@ -1,5 +1,21 @@
 package com.java8;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamQuestions {
 
+	public static void main(String[] args) {
+	int count=0;
+		List<Integer> evenList=Arrays.asList(1,3,5,6,7,8,21,54,68,87,65,45,34,22);
+		
+		// method reference.....
+		evenList.stream().filter(n -> n%2==0).forEach(System.out::print);
+		
+		//lambda Expression
+		evenList.stream().filter(n -> n%2==0).forEach(n->System.out.print(n + " "));
+		
+		
+		
+	}
 }
