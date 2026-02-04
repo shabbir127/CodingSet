c28a43d643603a83851736b23343fbc1d6ff4f1f
shabbirkhan <khanshabbir126@gmail.com>
2026-02-05 00:08:54 +0530

    done

--- src/com/java8/StreamQuestions.java
+++ src/com/java8/StreamQuestions.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+
 public class StreamQuestions {
 
 	public static void main(String[] args) {
@@ -25,5 +26,79 @@ public class StreamQuestions {
 		System.out.println("\n second max value "+SecondMax);
 		
 		
+		Student student1=new Student(1, 10000, "Shabbir", false);
+		Student student2=new Student(2, 34000, "Rohit", true);
+		Student student3=new Student(3, 67000, "Vivek", false);
+		Student student4=new Student(4, 780000, "Sujit", true);
+		Student student5=new Student(5, 89000, "Altaf", true);
+		Student student6=new Student(6, 33000, "Amit", false);
+		Student student7=new Student(7, 45000, "Dani", true);
+		Student student8=new Student(8, 45000, "Savita", true);
+		Student student9=new Student(9, 45000, "Ram", true);
+		Student student10=new Student(10, 45000, "Mahtest", false);
+		Student student11=new Student(11, 35000, "Shivam", true);
+		Student student12=new Student(13, 93000, "Ramesh", true);
+		Student student13=new Student(14, 25000, "Suresh", false);
+		Student student14=new Student(15, 33500, "Danish", true);
+		Student student15=new Student(16, 82330, "Jiberal", false);
+		
+		
+		List<Student>studentlList=Arrays.asList(student1,student2,student3,student4,
+				student5,student6,student7,student8,student9,student10,student11,
+				student12,student13,student14,student15);
+		
+		
+		List<Student> reStudents=studentlList.stream().filter(Student::isFees).filter(s-> s.getSalary() > 10000).sorted(Comparator.comparing(Student::getSalary).reversed()).toList();
+		reStudents.stream().forEach(System.out::println);
+		System.out.println(reStudents.stream().count());
+		
 	}
+}
+
+
+class Student{
+	
+	private int id;
+	private long salary;
+	private String name;
+	private boolean fees;
+	
+	public Student(int id, long salary, String name, boolean fees) {
+		super();
+		this.id = id;
+		this.salary = salary;
+		this.name = name;
+		this.fees = fees;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isFees() {
+		return fees;
+	}
+	public void setFees(boolean fees) {
+		this.fees = fees;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", salary=" + salary + ", name=" + name + ", fees=" + fees + "]";
+	}
+	
+	
+	
 }
