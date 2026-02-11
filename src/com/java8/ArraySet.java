06621a700c106a5b4434bb12e0e56ab3d3a6f56a
shabbirkhan <khanshabbir126@gmail.com>
2026-02-11 21:59:35 +0530

    commit

--- src/com/java8/ArraySet.java
+++ src/com/java8/ArraySet.java
@@ -2,12 +2,8 @@ package com.java8;
 
 public class ArraySet {
 	
-	
-	
-	public static void main(String[] args) {
-		
-		int arr[]= {20,23,14,25,26,26,27,39,38,40,40};
-		
+	public static void SecondLargest(int arr[]) {
+
 		int height=Integer.MIN_VALUE;
 		int SecongHeight=Integer.MIN_VALUE;
 		
@@ -22,6 +18,14 @@ public class ArraySet {
 			}
 		}
 		System.out.println("SecondHeight value is :- "+SecongHeight);
+
 	}
+	
+	public static void main(String[] args) {
+		
+		int arr[]= {20,23,14,25,26,26,27,39,38,40,40};
+		ArraySet.SecondLargest(arr);
+		
+			}
 
 }
