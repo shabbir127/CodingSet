f22500e896f54262e190b0812b69d4a5b54c0968
shabbirkhan <khanshabbir126@gmail.com>
2026-02-28 23:43:06 +0530

    commit done

--- src/com/java8/MapEx.java
+++ src/com/java8/MapEx.java
@@ -9,5 +9,8 @@ public class MapEx {
 		List<List<Integer>> numbers=Arrays.asList(Arrays.asList(1,2,34,4),Arrays.asList(12,45,6,6,6));
 		
 		numbers.stream().flatMap(s->s.stream()).forEach(System.out::print);
+		
+		
+		
 	}
 }
