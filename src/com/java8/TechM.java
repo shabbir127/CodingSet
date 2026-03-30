0fd59b5e51343fd9a0a2a329858ef41e3a1cdd04
shabbirkhan <khanshabbir126@gmail.com>
2026-03-31 00:07:21 +0530

    commit

--- src/com/java8/TechM.java
+++ src/com/java8/TechM.java
@@ -1,5 +1,118 @@
 package com.java8;
 
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class TechM {
+	void deadlock()
+	{
+		Object lock1 = new Object();
+        Object lock2 = new Object();
+
+        Thread t1 = new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println("T1 locked lock1");
+
+                try { Thread.sleep(100); } 
+                
+                catch (Exception e) {
+                	System.out.println("Exception....");
+                }
+
+                synchronized (lock2) {
+                    System.out.println("T1 locked lock2");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (lock2) {
+                System.out.println("T2 locked lock2");
+
+                try { Thread.sleep(100); } 
+                catch (Exception e) {}
+
+                synchronized (lock1) {
+                    System.out.println("T2 locked lock1");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+	public static void main(String[] args) {
+		Set<Employe> set=new HashSet<>();
+		HashSet<Employe> hset=new HashSet<>();
+		
+		Employe e1=new Employe();
+		Employe e2=new Employe();
+		
+		e1.setAge(1);
+		e1.setName("Shabbir");
+		e2.setAge(1);
+		e2.setName("khan");
+		set.add(e2);
+		set.add(e1);
+		
+		hset.add(e2);
+		hset.add(e1);
+		
+		System.out.println("hashset "+hset.size());
+		System.out.println(set);
+		
+		HashMap<Integer, Employe> map=new HashMap<>();
+		map.put(1, e1);
+		map.put(1, e2);
+		
+		TechM m=new TechM();
+		m.deadlock();
+		
+		System.out.println(map);
+		
+	}
 
 }
+
+class Employe{
+	
+	private int age;
+	private String name;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	@Override
+	public String toString() {
+		return "Employe [age=" + age + ", name=" + name + "]";
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employe other = (Employe) obj;
+		return age == other.age && Objects.equals(name, other.name);
+	}
+	
+}
+	
